7c72e353fd83220dc8eaf10aaa0afc709882c165
VishwajeethV <74006085+VishwajeethV@users.noreply.github.com>
2020-12-28 17:43:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 function setup() {
-    canvas=createCanvas(300,300);
+    canvas=createCanvas(400,400);
     canvas.center();
     video=createCapture(VIDEO);
-    video.size(300,300);
+    video.size(400,400);
+    video.position(50,120);
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',getposes);
 }
 
+
+
 function draw() {
-   
+   background("#aaff80");
 }
+
+
+
+function modelLoaded() {
+    console.log("posenet is intialized");
+}
+
+
+
+function getposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
+}
+
