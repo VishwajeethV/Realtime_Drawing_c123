e8e3ad6661e531adce6c2dbbdd0fd1202b970943
VishwajeethV <74006085+VishwajeethV@users.noreply.github.com>
2020-12-31 17:37:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+nosex=0;
+nosey=0;
+leftwristx=0;
+rightwristx=0;
+difference=0;
+
+
+
 function setup() {
     canvas=createCanvas(400,400);
     canvas.center();
@@ -12,6 +20,10 @@ function setup() {
 
 function draw() {
    background("#aaff80");
+   fill("goldenrod");
+   stroke("white");
+   square(nosex,nosey,difference);
+   document.getElementById("square_side").innerHTML="Length of square is "+difference+" px";
 }
 
 
@@ -25,6 +37,13 @@ function modelLoaded() {
 function getposes(results) {
     if(results.length > 0) {
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log(nosex,nosey);
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log(difference);
     }
 }
 
